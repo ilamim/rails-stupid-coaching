940b4edd3975286cf4567a7ffb6f06ddfd2f60e0
Isabel Lamim <lamim@protonmail.com>
2022-08-12 11:46:09 +0200

    functional integration, just need to improve the view and the controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,5 +3,21 @@ class QuestionsController < ApplicationController
   end
 
   def answer
+    if params[:user_question].downcase == ('i am going to work right now!')
+      @coach_answer = 'Great!'
+    else
+      @coach_answer = "I don't care, get dressed and go to work!"
+    end
+  # def coach_answer_enhanced(your_message)
+  #   if your_message != your_message.upcase
+  #     return coach_answer(your_message)
+  #   elsif your_message == "I AM GOING TO WORK RIGHT NOW!"
+  #     return ""
+  #   elsif your_message[-1] == "?"
+  #     return "I can feel your motivation! Silly question, get dressed and go to work!"
+  #   else
+  #     return "I can feel your motivation! I don't care, get dressed and go to work!"
+  #   end
+  return @coach_answer
   end
 end
