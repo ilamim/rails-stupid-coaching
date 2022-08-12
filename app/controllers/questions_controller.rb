36f5d067d4dcad795537ab3a04976f662bc15430
Isabel Lamim <lamim@protonmail.com>
2022-08-12 12:02:24 +0200

    all options functional, missing view improvements

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,21 +3,13 @@ class QuestionsController < ApplicationController
   end
 
   def answer
-    if params[:user_question].downcase == ('i am going to work right now!')
-      @coach_answer = 'Great!'
+    if params[:user_question].upcase == 'I AM GOING TO WORK RIGHT NOW!'
+      @coach_answer = ''
+    elsif params[:user_question][-1] == '?'
+      @coach_answer = 'Silly question, get dressed and go to work!'
     else
       @coach_answer = "I don't care, get dressed and go to work!"
     end
-  # def coach_answer_enhanced(your_message)
-  #   if your_message != your_message.upcase
-  #     return coach_answer(your_message)
-  #   elsif your_message == "I AM GOING TO WORK RIGHT NOW!"
-  #     return ""
-  #   elsif your_message[-1] == "?"
-  #     return "I can feel your motivation! Silly question, get dressed and go to work!"
-  #   else
-  #     return "I can feel your motivation! I don't care, get dressed and go to work!"
-  #   end
-  return @coach_answer
+    @coach_answer
   end
 end
